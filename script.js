c0650c569e1ec196cdec18d1f8fc026cc96044d3
José Vitor <josevitordesouza0@gmail.com>
2026-04-22 19:55:18 -0300

    mudanca em tudo

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 function whatsapp() {
-    let numero = "5583986477047"; // coloca teu número aqui
-    let mensagem = "Olá, quero um orçamento de móveis projetados!";
-    
-    let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
-    window.open(url, "_blank");
+    let numero = "5583999999999"; // coloca teu número
+    let mensagem = "Olá, quero um orçamento!";
+    window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`);
 }
 
 function toggleMenu() {
     let menu = document.getElementById("menu");
-
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
